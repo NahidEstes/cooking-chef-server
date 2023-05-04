e814428991c33e48d5e6c8d7461c98bd75d53ba2
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-05-04 09:34:39 +0600

    updated image data

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const port = process.env.PORT || 5000;
 
 const chefInformation = require("./data/chef-data.json");
 
-// const touristSpot = require("./data/travel-data.json");
-// const travelDetails = require("./data/travel-details-data.json");
-
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -18,40 +15,12 @@ app.get("/chef-info", (req, res) => {
   res.send(chefInformation);
 });
 
-// app.get("/travel-details", (req, res) => {
-//   res.send(travelDetails);
-// });
-
 app.get("/chef-info/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const chefDetails = chefInformation.find((n) => n.id === id);
   res.send(chefDetails);
   console.log(id);
 });
-
-// app.get("/news/:id", (req, res) => {
-//   const id = req.params.id;
-//   const selectedNews = news.find((n) => n._id === id);
-//   res.send(selectedNews);
-// });
-
-// -------------
-
-// app.get("/news/:id", (req, res) => {
-//   const id = req.params.id;
-//   const selectedNews = news.find((n) => n._id === id);
-//   res.send(selectedNews);
-// });
-
-// app.get("/categories/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   if (id === 0) {
-//     res.send(news);
-//   } else {
-//     const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
-//     res.send(categoryNews);
-//   }
-// });
 
 app.listen(port, () => {
   console.log(`Food Chef is running on port: ${port}`);
